b5cf3e5005c63ee10334ec1cb5219b8e29505a21
stefan judis <stefanjudis@gmail.com>
2013-07-02 23:02:08 +0200

    added first new builds functionality + tests for that

---
+++ test/buildsTest.js
@@ -0,0 +1,77 @@
+'use strict';
+
+module.exports = {
+  setUp : function( callback ) {
+    this.Travalizit = require( '../lib/travalizit' );
+    this.request    = require( 'request' );
+
+    callback();
+  },
+
+
+  tearDown : function( callback ) {
+    callback();
+  },
+
+
+  init : function( test ) {
+    var builds = new this.Travalizit().builds();
+
+    test.strictEqual( typeof builds, 'object' );
+
+    test.done();
+  },
+
+  all : {
+    repoIsDefined : function( test ) {
+      var builds = new this.Travalizit().builds( '510697' ),
+          request = this.request,
+          callback = function( error, request, response ) {
+            console.log( error );
+            console.log( response );
+          };
+
+      this.request.get = function() {
+        test.strictEqual( arguments.length, 2);
+
+        test.strictEqual( typeof arguments[ 0 ], 'object' );
+        test.strictEqual( arguments[ 0 ].qs.repository_id, '510697' );
+        test.strictEqual( arguments[ 0 ].qs.event_type, 'push' );
+
+        test.strictEqual( typeof arguments[ 1 ], 'function' );
+        test.strictEqual( arguments[ 1 ], callback );
+
+        test.done();
+      };
+
+      builds.all( callback )
+
+      this.request = request;
+    },
+    repoIsUndefined : function( test ) {
+      var builds = new this.Travalizit().builds( '' ),
+          request = this.request,
+          callback = function( error, request, response ) {
+            console.log( error );
+            console.log( response );
+          };
+
+      this.request.get = function() {
+        test.strictEqual( arguments.length, 2);
+
+        test.strictEqual( typeof arguments[ 0 ], 'object' );
+        test.strictEqual( arguments[ 0 ].qs.repository_id, '' );
+        test.strictEqual( arguments[ 0 ].qs.event_type, 'push' );
+
+        test.strictEqual( typeof arguments[ 1 ], 'function' );
+        test.strictEqual( arguments[ 1 ], callback );
+
+        test.done();
+      };
+
+      builds.all( callback )
+
+      this.request = request;
+    }
+  }
+};
